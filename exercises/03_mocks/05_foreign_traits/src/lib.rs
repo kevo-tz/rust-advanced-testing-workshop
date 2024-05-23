20e6307d11e25e8f6de3423331287077d89f95c0
kevo-tz <me@kevo.co.tz>
2024-05-23 17:55:19 +0000

    (03) mocks - (05) foreign_traits solution

--- exercises/03_mocks/05_foreign_traits/src/lib.rs
+++ exercises/03_mocks/05_foreign_traits/src/lib.rs
@@ -2,6 +2,13 @@ use std::str::FromStr;
 
 mockall::mock! {
     // TODO!
+    Parsed {}
+
+    impl FromStr for Parsed {
+        type Err = Box<dyn std::error::Error>;
+        fn from_str(s: &str) -> Result<Self, <MockParsed as FromStr>::Err>;
+    }
+
 }
 
 #[cfg(test)]
