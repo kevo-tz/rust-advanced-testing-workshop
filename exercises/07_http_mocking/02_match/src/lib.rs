e2eb3d7039ec9d9ceadee9db800e899f569e56e8
kevo-tz <me@kevo.co.tz>
2024-06-25 18:59:26 +0000

    (07) http_mocking - (02) match solution

--- exercises/07_http_mocking/02_match/src/lib.rs
+++ exercises/07_http_mocking/02_match/src/lib.rs
@@ -1,10 +1,19 @@
 use wiremock::{Match, Request};
 
+use wiremock::matchers::{body_json_schema, header, method};
+
 struct WellFormedJson;
 
 impl Match for WellFormedJson {
     fn matches(&self, request: &Request) -> bool {
-        todo!("Implement me!")
+        // The method is POST
+        method("POST").matches(request)
+        // The Content-Type header is present and set to application/json
+        && header("Content-Type", "application/json").matches(request)
+        // The Content-Length header is set and its value matches the length of the request body (in bytes)
+        && header("content-length", request.body.len()).matches(request)
+        // The request body is a valid JSON object
+        && body_json_schema::<serde_json::Value>(request)
     }
 }
 
